c3362a0dc6408a35bae2d875765ed81e3a4a9379
Vinay N <vinay.n@profinch.com>
2020-02-06 12:51:21 +0530

    new file added

---
+++ usetinput.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class usetinput {
+    public static void main(String[] args){
+        Scanner sc =new Scanner(System.in);
+        String str = sc.nextLine();
+        System.out.println("you enter:" +str);
+
+        int a =sc.nextInt();
+        System.out.println("you enter:"+a);
+    }
+}
