7b2352e86a39fc6f40befa7f656ceba5b19c0557
Vinay N <vinay.n@profinch.com>
2020-02-10 12:42:19 +0530

    new files added

---
+++ Variables.java
@@ -0,0 +1,13 @@
+public class Variables
+{
+    public static void main(String[] args)
+    {
+        // declare the variable
+        int firstVariable;
+
+        //initialize the variable
+        firstVariable=10;
+
+        System.out.println(firstVariable);
+    }
+}
